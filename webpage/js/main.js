14cc19b18c2e541249d29632553a25ac412503f1
Purnima <purnima.valluri@gmail.com>
2019-01-09 15:56:54 -0600

    Version 2.0-Design option 2

--- webpage/js/main.js
+++ webpage/js/main.js
@@ -20,41 +20,7 @@ $(document).ready(function(){
          navMain.collapse('hide');
      });
  });
-        
- $('.ml1 .letters').each(function(){
-  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-});
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml1 .letter',
-    scale: [0.3,1],
-    opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: function(el, i) {
-      return 70 * (i+1)
-    }
-  }).add({
-    targets: '.ml1 .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 800,
-    offset: '-=875',
-    delay: function(el, i, l) {
-      return 80 * (l - i);
-    }
-  }).add({
-    targets: '.ml1',
-    opacity: 1,
-    duration: 1100,
-    easing: "easeOutExpo",
-    delay: 1100
-  });
-  
-  $(".navbar a").click(function(){
+ $(".navbar a").click(function(){
   $("body,html").animate({
    scrollTop:$("#" + $(this).data('value')).offset().top
   },1000)
